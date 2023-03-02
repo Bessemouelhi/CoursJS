d757617a6b7bcbfcbaa60f2cdeedad6241380f1d
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-03-02 09:40:05 +0100

    add paralax

--- paralax.js
+++ paralax.js
@@ -1,7 +1,12 @@
 const img = document.querySelectorAll('img');
 window.addEventListener('scroll', function(){
     let lastPos = this.window.scrollY;
-    img.forEach(element => {
+    // img.forEach(element => {
+    //     element.style.left = (Math.floor(lastPos / 5)) + '%';
+    // })
+    for (let i = 0; i < img.length; i++) {
+        const element = img[i];
         element.style.left = (Math.floor(lastPos / 5)) + '%';
-    })
+        element.style.borderRadius = (Math.floor(lastPos / 10)) + '%';
+    }
 })
