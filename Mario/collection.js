134d6113dcdd854fa97c88e8bb994863eeca2f77
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-03-24 16:58:34 +0100

    update popup

--- Mario/collection.js
+++ Mario/collection.js
@@ -30,11 +30,11 @@ function mario(titre, date, platform, description, logo) {
     this.logo = logo;
 }
 
-let mario1 = new mario("Super mario Bros.", 1985, "NES", "Super Mario Bros. (スーパーマリオブラザーズ, Sūpā Mario Burazāzu?) est un jeu vidéo de plates-formes à défilement horizontal développé par Nintendo R&D4 et édité par Nintendo. Il est sorti sur Famicom au Japon en 1985 puis sur Nintendo Entertainment System en Amérique du Nord la même année et en 1987 en Europe. Il s'agit du premier jeu de la série Super Mario.");
+let mario1 = new mario("Super mario Bros.", 1985, "NES", "Super Mario Bros. (スーパーマリオブラザーズ, Sūpā Mario Burazāzu?) est un jeu vidéo de plates-formes à défilement horizontal développé par Nintendo R&D4 et édité par Nintendo. Il est sorti sur Famicom au Japon en 1985 puis sur Nintendo Entertainment System en Amérique du Nord la même année et en 1987 en Europe. Il s'agit du premier jeu de la série Super Mario.", "mario1.svg");
 
-let mario2 = new mario("Super mario Bros. 2", 1988, "NES", "Super Mario Bros. 2 (ou Super Mario USA au Japon) est un jeu vidéo de plates-formes développé par Nintendo R&D4 et édité par Nintendo. Il sort sur Nintendo Entertainment System en 1988 en Amérique du Nord, en 1989 en Europe puis en 1992 au Japon.");
+let mario2 = new mario("Super mario Bros. 2", 1988, "NES", "Super Mario Bros. 2 (ou Super Mario USA au Japon) est un jeu vidéo de plates-formes développé par Nintendo R&D4 et édité par Nintendo. Il sort sur Nintendo Entertainment System en 1988 en Amérique du Nord, en 1989 en Europe puis en 1992 au Japon.", "mario2.jpg");
 
-let mario3 = new mario("Super mario Bros. 3", 1988, "NES", "Super Mario Bros. 3 (スーパーマリオブラザーズ3, Sūpā Mario Burazāzu Surī?) est un jeu vidéo de plates-formes développé et édité par Nintendo sur Nintendo Entertainment System. Il s'agit du troisième volet de la série Super Mario et il est commercialisé en 1988 au Japon, en 1990 aux États-Unis et en 1991 en Europe. Le développement est pris en charge par Nintendo Entertainment Analysis & Development sous la houlette de Shigeru Miyamoto, qui dirige le jeu avec Takashi Tezuka et Hiroshi Yamauchi.");
+let mario3 = new mario("Super mario Bros. 3", 1988, "NES", "Super Mario Bros. 3 (スーパーマリオブラザーズ3, Sūpā Mario Burazāzu Surī?) est un jeu vidéo de plates-formes développé et édité par Nintendo sur Nintendo Entertainment System. Il s'agit du troisième volet de la série Super Mario et il est commercialisé en 1988 au Japon, en 1990 aux États-Unis et en 1991 en Europe. Le développement est pris en charge par Nintendo Entertainment Analysis & Development sous la houlette de Shigeru Miyamoto, qui dirige le jeu avec Takashi Tezuka et Hiroshi Yamauchi.", "mario3.svg");
 
 let mario4 = new mario("Super mario Land", 1989, "Game Boy", "");
 let mario5 = new mario("Super mario World", 1990, "Super Famicom", "");
@@ -56,13 +56,41 @@ myCollec.set(8, mario8);
 myCollec.set(9, mario9);
 myCollec.set(10, mario10);
 
-let maCollection = {...{mario1}, ...{mario2}, ...{mario3}, 
-                    ...{mario4}, ...{mario5}, ...{mario6}, 
-                    ...{mario7}, ...{mario8}, ...{mario9}, ...{mario10}};
+let maCollection = {
+    ...{
+        mario1
+    },
+    ...{
+        mario2
+    },
+    ...{
+        mario3
+    },
+    ...{
+        mario4
+    },
+    ...{
+        mario5
+    },
+    ...{
+        mario6
+    },
+    ...{
+        mario7
+    },
+    ...{
+        mario8
+    },
+    ...{
+        mario9
+    },
+    ...{
+        mario10
+    }
+};
 console.log(maCollection);
 
-for (const key in maCollection)
-{
+for (const key in maCollection) {
     console.log(maCollection[key]); //syntaxe obligatoire lorsque la clé est dynamique
 };
 
@@ -70,13 +98,13 @@ console.log(">>>>>>>>>>>>>>>>");
 
 let myBody = document.querySelector('body');
 
-Object.values(maCollection).map( (el, index) => {
+Object.values(maCollection).map((el, index) => {
     console.log(el.titre);
     let inHtml = "";
     inHtml += "<div class='jeu'><h2>" + el.titre + "</h2>";
     inHtml += "<h3>" + el.platform + "</h3>";
     inHtml += "<h4>" + el.date + "</h4>";
-    inHtml += "<button class='POPUPjeu' id='mario" + (index+1) + "'>OPEN</button></div>";
+    inHtml += "<button class='POPUPjeu' id='mario" + (index + 1) + "'>OPEN</button></div>";
 
     myBody.innerHTML += inHtml;
 })
@@ -97,6 +125,7 @@ let popTitre = document.getElementById('titre');
 let popDate = document.getElementById('date');
 let popPlatform = document.getElementById('platform');
 let popDesc = document.getElementById('description');
+let popLogo = document.getElementById('logo');
 
 // When the user clicks the button, open the modal 
 // btn.onclick = function() {
@@ -106,24 +135,25 @@ let popDesc = document.getElementById('description');
 for (let index = 0; index < btnPop.length; index++) {
     let key = btnPop[index].id;
     console.log(key);
-    btnPop[index].addEventListener('click' ,(e) => {
+    btnPop[index].addEventListener('click', (e) => {
         console.log(maCollection[key].titre);
         modal.style.display = "block";
         popTitre.innerText = maCollection[key].titre;
         popDate.innerText = maCollection[key].date;
         popPlatform.innerText = maCollection[key].platform;
         popDesc.innerText = maCollection[key].description;
+        popLogo.src = 'img/'+maCollection[key].logo;
     })
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
